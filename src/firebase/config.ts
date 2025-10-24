2dce85d74588860214db7ef3483767b078f8cef7
Shockz <dhanwinkiumar@gmail.com>
2025-10-24 03:26:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3803799571-93019",
+  "appId": "1:648055464133:web:42a073eed26e53516fd0ac",
+  "apiKey": "AIzaSyDXXOJRMmpElv7JqiqFDH3-Dy67FhJ1ksE",
+  "authDomain": "studio-3803799571-93019.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "648055464133"
+};
